1a6a3907503aea38cb238484a0852251b2c2cd15
Philip Ashton <philligan1@gmail.com>
2024-09-22 19:16:49 -0500

    #'8,9,10

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -39,10 +39,17 @@ console.log (food);
 
 // 8 - Create a variable called `pets` and set it to the value of the number of pets you have
 //     Console log the value of `pets`
-let 
+let pets = 2;
+console.log (pets);
+
 // 9 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
+let friendsPets = 3;
+console.log (friendsPets);
 
 // 10 - Add two pets to your `pets` variable
+pets = pets + 2;
+console.log (pets);
+
 
 // 11 - Create a constant variable called `allowedPets` and set it to a number value of your choice
 
